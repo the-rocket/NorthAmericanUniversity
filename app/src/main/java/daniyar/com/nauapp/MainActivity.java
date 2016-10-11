5d3a7d7a1a97638ea2adbe3c0cb0bce525b12070
Daniyar Kayirbolatov <daniyarkayirbolatov@gmail.com>
2016-10-10 20:09:58 -0500

    Code Improved

--- app/src/main/java/daniyar/com/nauapp/MainActivity.java
+++ app/src/main/java/daniyar/com/nauapp/MainActivity.java
@@ -3,42 +3,45 @@ package daniyar.com.nauapp;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import daniyar.com.nauapp.Fragments.MoodleFragment;
 import daniyar.com.nauapp.Fragments.NewsFragment;
 import daniyar.com.nauapp.Fragments.StudentFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     private String MoodleFragment = "Moodle";
     private String NewsFragment = "News";
     private String StudentFragment = "Student";
 
-    TabLayout tabs;
-    ViewPager viewPager;
-
-    @BindView(R.id.tool) Toolbar tool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        setSupportActionBar(tool);
+        init();
+    }
+
+    public void init() {
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
         tabs = (TabLayout) findViewById(R.id.tabs);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-        }
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new MoodleFragment(), "Moodle");
-        adapter.addFragment(new NewsFragment(), "News");
-        adapter.addFragment(new StudentFragment(), "Student");
+
+        initViews();
+
+    }
+
+    public void initViews() {
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MoodleFragment(), MoodleFragment);
+        adapter.addFragment(new NewsFragment(), NewsFragment);
+        adapter.addFragment(new StudentFragment(), StudentFragment);
         viewPager.setAdapter(adapter);
         tabs.setupWithViewPager(viewPager);
     }
+
+    protected int getLayout() {
+        return R.layout.activity_main;
+    }
+
 }
